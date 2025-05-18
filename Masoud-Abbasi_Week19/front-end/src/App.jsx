73cc698367e29e95265d65142dd8adaf424b2b6c
MK <masoud.abbasi.71@gmail.com>
2025-05-18 20:41:45 +0330

    Add page components and complete page404

--- Masoud-Abbasi_Week19/front-end/src/App.jsx
+++ Masoud-Abbasi_Week19/front-end/src/App.jsx
@@ -1,5 +1,36 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Panel from "./pages/Panel.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Page404 from "./pages/Page404.jsx";
+import { useApp } from "./context/appProvider";
+
+const queryClient = new QueryClient();
 function App() {
-  return <></>;
+  const { state } = useApp();
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                state.isAuthenticated ? <Panel /> : <Navigate to="/login" />
+              }
+            />
+            <Route
+              path="/login"
+              element={state.isAuthenticated ? <Navigate to="/" /> : <Login />}
+            />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<Page404 />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
--- Masoud-Abbasi_Week19/front-end/src/main.jsx
+++ Masoud-Abbasi_Week19/front-end/src/main.jsx
@@ -1,8 +1,24 @@
-import React from "react";
-import ReactDom from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { ToastContainer } from "react-toastify";
+
+import AppProvider from "./context/appProvider.jsx";
 import App from "./App.jsx";
+import "./Global.css";
 
-ReactDom.createRoot(document.getElementById("root")).render(
-			<App />
-
+createRoot(document.getElementById("root")).render(
+  <AppProvider>
+    <App />
+    <ToastContainer
+      position="top-center"
+      autoClose={1500}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={true}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+  </AppProvider>
 );
